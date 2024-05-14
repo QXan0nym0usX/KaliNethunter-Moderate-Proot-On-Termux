53b844815c51fdc67371d0137b84d13ac0e82478
Sota4Ever <151886690+Sota4Ever@users.noreply.github.com>
2024-05-14 05:24:37 -0500

    Update kali.sh

--- AutomaticInstall/kali.sh
+++ AutomaticInstall/kali.sh
@@ -1,3 +1,4 @@
 DISTRO_NAME='Kali Nethunter'
 DISTRO_COMMENT="Kali modified by me :)"
 TARBALL_URL['aarch64']="https://archive.org/download/kali.tar/kali.tar.xz"
+TARBALL_SHA256['aarch64']="f1d6152cd9d87f7436ff535f32ef20189753dccf424364fcf039d0e818f142a7"
